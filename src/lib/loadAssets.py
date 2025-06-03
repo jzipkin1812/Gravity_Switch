ec08aab86daad8cf4a7d3f75c1cb21523d344be7
jzipkin1812 <javinzipkin@gmail.com>
2025-06-03 15:06:27 -0700

    multi-OS path join

--- src/lib/loadAssets.py
+++ src/lib/loadAssets.py
@@ -1,11 +1,13 @@
 import pygame
 import os
 from . import utility as u
+
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.init()
 
-path = os.getcwd() + "\\lib\\assets\\"
-titleImage = pygame.image.load(path + "titleScreen.png")
-levelSelectImage = pygame.image.load(path + "levelSelect.png")
-gameOverImage = pygame.image.load(path + "gameover.png")
-howToPlayImage = pygame.image.load(path + "howToPlay.png")
+path = os.path.join(os.getcwd(), "lib", "assets")
+
+titleImage = pygame.image.load(os.path.join(path, "titleScreen.png"))
+levelSelectImage = pygame.image.load(os.path.join(path, "levelSelect.png"))
+gameOverImage = pygame.image.load(os.path.join(path, "gameover.png"))
+howToPlayImage = pygame.image.load(os.path.join(path, "howToPlay.png"))
